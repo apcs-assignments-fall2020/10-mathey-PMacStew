004c9a30c33c2ac20ddf360910b15a735b3f6f9f
PMacStew <23pmcclure@athenian.org>
2020-10-15 20:37:06 +0000

    methods

--- src/main/java/Mathey.java
+++ src/main/java/Mathey.java
@@ -1,3 +1,36 @@
 public class Mathey {
-
+    public static int max(int x, int y) {
+        return Math.max(x, y);
+    }
+    public static double max(double x, double y) {
+        return Math.max(x, y);
+    }
+    public static int max(int x, int y, int z) {
+        return max(x, max(y, z));
+    }
+    public static double max(double x, double y, double z) {
+        return max(x, max(y, z));
+    }
+    public static int min(int x, int y) {
+        return Math.min(x, y);
+    }
+    public static double min(double x, double y) {
+        return Math.min(x, y);
+    }
+    public static int min(int x, int y, int z) {
+        return min(x, min(y, z));
+    }
+    public static double min(double x, double y, double z) {
+        return min(x, min(y, z));
+    }
+    public static int randomInteger(int a, int b) {
+        return (int) (Math.random() * (b - a + 1)) + a; 
+    }
+    public static int randomInteger(int a) {
+        return (int) (Math.random() * (a + 1));
+    }
+    public static void main(String[] args) {
+        System.out.println(randomInteger(1, 2));
+        System.out.println(randomInteger(1));
+    }
 }
--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,13 +1,21 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class MyMain {
 
-    public static int someMethod() {
-        return 1;
+    public static int[] someMethod() {
+        int[] arr = new int[3];
+        for (int i = 0; i < 3; i++) 
+            arr[i] = (int) (Math.random() * 11) + 10;
+        Arrays.sort(arr);
+        return arr;
     }
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] arr = someMethod();
+        System.out.println("The three random numbers are " + arr[0] + ", " + arr[1] + ", " + arr[2]);
+        System.out.println("The smallest number is " + arr[0]);
+        System.out.println("The largest number is " + arr[2]);
     }
 
 }
